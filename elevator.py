5ab0624c251097eddfff706c43ccdbc4c0c3aab0
Crazy-Waffle-Man <benjamin.shifflet@gmail.com>
2025-08-22 11:01:25 -0400

    Remove @info, add main loop

--- elevator.py
+++ elevator.py
@@ -1,48 +1,51 @@
 from time import sleep
+
 from typing import List
 import logging
+import threading
 
 
-def info(func):
-    def wrapper(*args, **kwargs):
-        logging.info(f"Calling {func.__name__}")
-        result = func(*args, **kwargs)
-        logging.info(f"{func.__name__} completed")
-        return result
-    return wrapper
+
 
 
 class Person:
-    @info
+    
     def __init__(self, floor: int):
         self.floor: int = floor
         self.in_elevator: bool = False
+        self.elevator_in: List['Elevator'] = [] # not in an elevator yet, use a list so we can have them in no elevator.
     def __repr__(self) -> str:
         return f"Person(floor={self.floor}, in_elevator={self.in_elevator})"
+    def push_elevator_button(self, elevator: 'Elevator') -> None: # so that we can use Elevator before definition
+        elevator.travel(self.floor)
     
 class Elevator:
-    @info
+    
     def __init__(self):
         self.capacity: int = 10
         self.current_floor: int = 0
         self.people: List[Person] = []
     
-    @info
+    
     def add_person(self, person: Person) -> bool:
         if (len(self.people) < self.capacity) and not person.in_elevator:
             self.people.append(person)
             person.in_elevator = True
+            person.elevator_in = [self]
             return True
         else: return False
 
-    @info
+    
     def travel(self, floor: int) -> None:
+        global total_travel_time
         travel_time = (self.current_floor - floor) * 5
+        with threading.Lock():
+            total_travel_time += abs(travel_time)
         self.current_floor = floor
-        sleep(abs(travel_time))
+        sleep(abs(travel_time) // 5)
         self.stop()
     
-    @info
+    
     def stop(self):
         for person in self.people:
             if person.floor == self.current_floor:
@@ -51,7 +54,7 @@ class Elevator:
 
 
 class Building:
-    @info
+    
     def __init__(self, num_elevators: int = 4):
         self.elevators: List[Elevator] = [Elevator() for _ in range(num_elevators)]
         self.people: List[Person] = []
@@ -68,11 +71,12 @@ class Building:
         for _ in range(20):
             self.people.append(Person(6))
 
-    @info
+    
     def load_elevator(self, elevator_index: int) -> None:
         new_self_people = self.people.copy() #We do this so that we can modify self.people during iteration.
         for person in self.people:
-            if self.elevators[elevator_index].add_person(person) and self.elevators[elevator_index].current_floor == 0:
+            if len(self.elevators[elevator_index].people) < self.elevators[elevator_index].capacity and self.elevators[elevator_index].current_floor == 0:
+                self.elevators[elevator_index].add_person(person)
                 #Remove person from building list
                 new_self_people.remove(person)
             else:
@@ -85,3 +89,28 @@ class Building:
 
 building = Building()
 
+#main loop
+
+def elevator_behavior(elevator: Elevator):
+    while True:
+        if len(elevator.people) == 0:
+            #Go to ground floor and wait
+            elevator.travel(0)
+            break
+        else:
+            for person in elevator.people:
+                person.push_elevator_button(elevator)
+
+
+total_travel_time= 0
+
+while len(building.people) > 0:
+    building.load_all_elevators()
+    threads: List[threading.Thread] = []
+    for i, elevator in enumerate(building.elevators):
+        threads.append(threading.Thread(target=elevator_behavior, args=(elevator,)))
+        threads[i].start()
+    for thread in threads: thread.join()
+    print(len(building.people), "left on the ground floor.")
+
+print(f"Total travel time: {total_travel_time} seconds.")
